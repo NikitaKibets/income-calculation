40ebd1c48a58dc3fca01f9d62fe8832726471b31
mike wazovski <iwazovski@gmail.com>
2020-04-07 23:19:27 +0300

    Практика DOM + вёрстка

--- section2_basics/lesson5/src/js/main.js
+++ section2_basics/lesson5/src/js/main.js
@@ -78,3 +78,17 @@ let appData = {
 for (let key in appData) {
   console.log('Наша программа включает в себя данные: ' + key);
 }
+
+let start = document.getElementById('start'); // начать рассчёт 1шт
+let resultTable = document.getElementsByClassName('.result-table'); // блоки справа, по нечётному индексу 16 шт
+let expensesItemInput = document.getElementsByClassName('.expenses-item'); // input с обязательныит расходами 4шт
+let expensesItemBtn = document.querySelectorAll('.expenses-item-btn') // button "утвердить" 2шт
+let countBudgetBtn = document.querySelector('.count-budget-btn'); // кнопка рассчитать 1шт
+let optionalExpensesItemInput = document.querySelectorAll('.optionalexpenses-item'); // необязательные расходы 3шт
+let chooseIncome = document.querySelector('.choose-income'); // статьи возможного дохода
+let checkbox = document.getElementById('savings'); // Накопления (точечка, галочка)
+let chooseSum = document.querySelector('.choose-sum'); // сумма накопления
+let choosePercent = document.querySelector('.choose-percent'); // проценты
+let year = document.querySelector('.year');
+let month = document.querySelector('.month');
+let day = document.querySelector('.day');
